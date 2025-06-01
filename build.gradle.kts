2b87892f6f1e9817234f59bf791c6f4d57715feb
zcg9783 <zcg5665@126.com>
2025-06-01 18:18:18 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ cmaker {
         )
         cFlags.addAll(flags)
         cppFlags.addAll(flags)
-        abiFilters("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
+        abiFilters("arm64-v8a")
     }
     buildTypes {
         if (it.name == "release") {
