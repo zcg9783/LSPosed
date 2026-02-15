0a219d0b87edc92da0f19c7b8097b091129625c6
JingMatrix <jingmatrix@gmail.com>
2026-02-15 02:54:03 +0100

    Update build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,12 @@
-/*
- * This file is part of LSPosed.
- *
- * LSPosed is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * LSPosed is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with LSPosed.  If not, see <https://www.gnu.org/licenses/>.
- *
- * Copyright (C) 2021 - 2022 LSPosed Contributors
- */
-
 import com.android.build.api.dsl.ApplicationDefaultConfig
 import com.android.build.api.dsl.CommonExtension
 import com.android.build.gradle.api.AndroidBasePlugin
 import com.ncorti.ktfmt.gradle.tasks.KtfmtFormatTask
+import java.io.ByteArrayOutputStream
+import javax.inject.Inject
+import org.gradle.api.provider.ValueSource
+import org.gradle.api.provider.ValueSourceParameters
+import org.gradle.process.ExecOperations
 
 plugins {
     alias(libs.plugins.lsplugin.cmaker)
@@ -31,6 +17,53 @@ plugins {
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.ktfmt)
 }
+
+/** A ValueSource that executes 'git rev-list --count' to get the total commit count. */
+abstract class GitCommitCountValueSource : ValueSource<String, ValueSourceParameters.None> {
+    @get:Inject abstract val execOperations: ExecOperations
+
+    override fun obtain(): String {
+        val output = ByteArrayOutputStream()
+        val result =
+            execOperations.exec {
+                commandLine("git", "rev-list", "--count", "refs/remotes/origin/master")
+                standardOutput = output
+                isIgnoreExitValue = true
+            }
+        // Return the count if successful, otherwise a default of "1".
+        return if (result.exitValue == 0 && output.toString().isNotBlank()) {
+            output.toString().trim()
+        } else {
+            "1"
+        }
+    }
+}
+
+/** A ValueSource that executes 'git tag' to get the latest version tag. */
+abstract class GitLatestTagValueSource : ValueSource<String, ValueSourceParameters.None> {
+    @get:Inject abstract val execOperations: ExecOperations
+
+    override fun obtain(): String {
+        val output = ByteArrayOutputStream()
+        val result =
+            execOperations.exec {
+                commandLine("git", "tag", "--list", "--sort=-v:refname")
+                standardOutput = output
+                isIgnoreExitValue = true
+            }
+        // If successful, parse the first line. Provide a default if no tags are found.
+        return if (result.exitValue == 0 && output.toString().isNotBlank()) {
+            output.toString().lineSequence().first().removePrefix("v")
+        } else {
+            "1.0"
+        }
+    }
+}
+
+// This defers the execution of the git commands and allows Gradle to cache the results.
+val versionCodeProvider by extra(providers.of(GitCommitCountValueSource::class.java) {})
+val versionNameProvider by extra(providers.of(GitLatestTagValueSource::class.java) {})
+
 
 val repo = jgit.repo()
 val commitCount = (repo?.commitCount("refs/remotes/origin/master") ?: 1) + 4200
